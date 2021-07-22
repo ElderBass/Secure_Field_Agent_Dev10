2bc6a35f1e453a8d1ef5f76993a2cc020f2e429f
Seth Zygarlicke <zygster11@gmail.com>
2021-07-22 10:13:44 -0500

    Added folders/files for Login, Logout, NotFound, and Errors. Added JSX to Login and Errors. Installed react-router-dom and provided routes to all components in main.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,50 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./components/HomePage";
+import NotFound from "./components/NotFound";
+import ViewAgents from "./components/ViewAgents";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import AddAgent from "./components/AddAgent";
+import UpdateAgent from "./components/UpdateAgent";
+import DeleteAgent from "./components/DeleteAgent";
+
 
 function App() {
   return (
+    <Router>
     <div className="mainContainer">
       <Header />
-      <HomePage/>
+
+      <Route exact path="/">
+        <HomePage />
+      </Route>
+      <Route path="*">
+        <NotFound />
+      </Route>
+      <Route exact path="/agents/all">
+        <ViewAgents />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <SignUp />
+      </Route>
+      <Route exact path="/agents/add">
+        <AddAgent />
+      </Route>
+      <Route exact path="/agents/edit/:id">
+        <UpdateAgent />
+      </Route>
+      <Route exact path="/agents/delete/:id">
+        <DeleteAgent />
+      </Route>
       <Footer/>
     </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Login/index.js
@@ -0,0 +1,95 @@
+import { useContext, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import AuthContext from '../AuthContext';
+
+import Errors from './Errors';
+
+const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  const auth = useContext(AuthContext);
+
+  const history = useHistory();
+
+  const usernameOnChangeHandler = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const passwordOnChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const user = {
+      username: username,
+      password: password
+    }
+
+    const init = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    };
+
+    fetch('http://localhost:5000/authenticate', init)
+      .then(response => {
+        if (response.status === 200) {
+          return response.json();
+        } else if (response.status === 403) {
+          return null;
+        }
+        return Promise.reject('Something unexpected went wrong :)');
+      })
+      .then(data => {
+        if (data) {
+          auth.login(data.jwt_token);
+          history.push("/");
+        } else {
+          setErrors(["Login Failed."]);
+        }
+      })
+      .catch(error => console.log(error));
+
+
+
+
+  };
+
+  return (
+    <div className="formContent" id="loginContent">
+        <h2>Don't worry, you were never here ;)</h2>
+        <form className="form" id="loginForm" onSubmit={formSubmitHandler}>
+          <div className="field">
+            <label for="userName">User Name:</label>
+            <input
+              className="inputField"
+              id="userName"
+              name="userName"
+              type="text"
+            />
+          </div>
+          <div className="field">
+            <label for="password">Password:</label>
+            <input
+              className="inputField"
+              id="password"
+              name="password"
+              type="password"
+            />
+          </div>
+          <div className="submitDiv">
+            <Link className="cancelLink" to="/">Cancel</Link>
+            <button className="submitBtn" type="submit">Login</button>
+          </div>
+        </form>
+      </div>
+  );
+}
+
+export default Login;
