03d27787684bc0c66fd3e9bf16f3ed83770bb1d5
Seth Zygarlicke <zygster11@gmail.com>
2021-07-23 15:15:05 -0500

    Successfully implemented delete function and deleted a couple spies from database.

--- client/src/components/DeleteAgent/index.js
+++ client/src/components/DeleteAgent/index.js
@@ -1,21 +1,84 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import { useHistory, useParams, Link } from 'react-router-dom';
 import classified from "../../images/classified.png";
+import AuthContext from '../../utils/AuthContext';
+
 
 import './style.css'
 
 
 const DeleteAgent = (props) => {
 
+    const [agent, setAgent] = useState({
+        firstName: "",
+        middleName: "",
+        lastName: "",
+        dob: "",
+        heightInInches: 36,
+    });
 
+    const auth = useContext(AuthContext);
+    const history = useHistory();
+    const { id } = useParams();
+
+    useEffect(() => {
+        const init = {
+            headers: {
+                "Authorization": `Bearer ${auth.user.token}`
+            }
+        };
+
+        fetch(`http://localhost:8080/api/agent/${id}`, init)
+            .then(res => {
+                if (res.status === 404) {
+                    return Promise.reject(`Received 404 Not Found for Spy ID: ${id}`);
+                } else {
+                    return res.json();
+                }
+            })
+            .then(data => {
+                setAgent({
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    middleName: data.middleName,
+                    dob: data.dob,
+                    heightInInches: data.heightInInches
+                })
+            })
+            .catch(err => console.log(err));
+    }, [id])
+
+    const handleDeleteSpy =() => {
+        const init = {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${auth.user.token}`
+            }
+        }
+
+        fetch(`http://localhost:8080/api/agent/${id}`, init)
+            .then(res => {
+                if (res.status === 204) {
+                    history.push("/agents/all");
+                }
+                else if (res.status === 404) {
+                    return Promise.reject(`Received 404 Not Found for Spy ID: ${id}`);
+                } else {
+                    return Promise.reject("We've been compromised. Aborting mission.");
+                }
+            })
+            .catch(err => console.log(err));
+    }
+    
     return (
 
-        <div id="deleteSpy">
+        <div id="deleteSpy" className="content">
             <h2>Redact Agent</h2>
             <section id="spyToDelete">
                 <div id="spyImageDiv">
                     <img
-                        src={props.agent.image ? props.agent.image : classified}
-                        alt={`${props.agent.firstName} ${props.agent.lastName}`}
+                        src={agent.image ? agent.image : classified}
+                        alt={`${agent.firstName} ${agent.middleName} ${agent.lastName}`}
                         width="300"
                     />
                 </div>
@@ -23,27 +86,31 @@ const DeleteAgent = (props) => {
                 <div id="spyInformation">
                     <div className="spyDetails">
                         <p className="spyField">First Name:</p>
-                        <p>{props.agent.firstName}</p>
+                        <p>{agent.firstName}</p>
+                    </div>
+                    <div className="spyDetails">
+                        <p className="spyField">Middle Name:</p>
+                        <p>{agent.middleName}</p>
                     </div>
                     <div className="spyDetails">
                         <p className="spyField">Last Name:</p>
-                        <p>{props.agent.lastName}</p>
+                        <p>{agent.lastName}</p>
                     </div>
                     <div className="spyDetails">
                         <p className="spyField">Date of Birth:</p>
-                        <p>{props.agent.dob}</p>
+                        <p>{agent.dob}</p>
                     </div>
                     <div className="spyDetails">
                         <p className="spyField">Height (Inches):</p>
-                        <p>{props.agent.heightInInches}</p>
+                        <p>{agent.heightInInches}</p>
                     </div>
                 </div>
             </section>
             <section id="confirmDelete">
-                <h2>Delete "{props.agent.firstName} {props.agent.lastName}" from Database?</h2>
+                <h2>Purge "{agent.firstName} {agent.middleName} {agent.lastName}" from Database?</h2>
                 <div class="submitDiv">
-                    <button id="cancelBtn" onClick={props.cancel}>Cancel</button>
-                    <button className="submitBtn" type="button" onClick={props.confirm}>Purge Spy</button>
+                    <Link id="cancelBtn" to="/agents/all">Cancel</Link>
+                    <button className="submitBtn" type="button" onClick={handleDeleteSpy}>Purge Spy</button>
                 </div>
             </section>
         </div>
